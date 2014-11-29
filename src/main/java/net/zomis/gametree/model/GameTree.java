4e607899483a29323df1fcec92929453f5390f70
Simon Forsberg <zomis2k@hotmail.com>
2014-11-30 00:00:29 +0100

    made changes to the entities

--- src/main/java/net/zomis/gametree/model/GameTree.java
+++ src/main/java/net/zomis/gametree/model/GameTree.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -26,11 +27,23 @@ public class GameTree {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer	gameid;
 	
+	@ManyToOne
+	private TreeUser author;
+	
+	private String name;
+	
 	@Temporal(value = TemporalType.TIMESTAMP)
 	private Date time;
 	
-	@OneToMany
+	@OneToMany(targetEntity = GameNode.class)
 	private List<GameNode> nodes;
-
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Integer getGameid() {
+		return gameid;
+	}
 	
 }
--- src/main/java/net/zomis/gametree/model/TreeUser.java
+++ src/main/java/net/zomis/gametree/model/TreeUser.java
@@ -6,13 +6,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class GameNode {
+public class TreeUser {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer id;
-	
-	
+	private Integer	userid;
+
 	private String name;
 	
 	public String getName() {
